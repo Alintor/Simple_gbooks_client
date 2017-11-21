8316716a8751960d9f6704d737ae16d6b4ac21b5
Alexandr Ovchinnikov <Alintorius@gmail.com>
2017-11-21 23:44:25 +0700

    Networking constants and requests

---
+++ Gbooks/NetworkingLayer/NetworkingConstants.swift
@@ -0,0 +1,12 @@
+
+struct NetworkingConstants {
+    static let baseUrl = "https://www.googleapis.com/books/v1"
+    static let volumes = "/volumes"
+    static let favorites = "/mylibrary/bookshelves/0"
+    static let addValume = "/addValume"
+    static let removeValume = "/removeVolume"
+    
+    static let volumeId = "volumeId"
+    static let searchString = "q"
+    static let authorization = "Authorization"
+}
